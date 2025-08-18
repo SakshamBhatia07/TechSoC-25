e82306fc3ba6362bf8b01bafca0e9bed543c8e19
SakshamBhatia-mnc-iiti <saksham.bhatia0828@gmail.com>
2025-08-18 10:55:59 +0530

    Made basic calc, (tan,itf) left

--- Problem_Statement_0/PS0.py
+++ Problem_Statement_0/PS0.py
@@ -1,3 +1,26 @@
+def factorial(n):
+  if( n == 0):
+      return 1
+  elif(n>0):
+      return n * factorial(n-1)
+def sin(a):
+  z=0
+  y=0
+  while(z<=10):
+    x=(((-1)**z)*(a**(2*z+1)))/factorial(2*z+1)
+    y=y+x
+    z=z+1
+  print(y)
+
+def cos(a):
+  z=0
+  y=0
+  while(z<=10):
+    x=(((-1)**(z))*(a**(2*z)))/factorial(2*z)
+    y=y+x
+    z=z+1
+  print(y)
+
 def Calculator(*num):
   if(b=="+"):
     print(a+c)
@@ -24,18 +47,17 @@ def Calculator(*num):
   elif(b=="exp"):
     print(2.7182818284590**a)
   elif(b=="sin"):
-    print(sin(a))
+    sin(a)
   elif(b=="cos"):
-    print(cos(a))
-  elif(b=="tan"):
-    print(tan(a))
+    cos(a)
 
-
-b = input("Enter the operator: ")
-if(b=="+" or b=="-" or b=="/" or b=="*" or b=="**" or b=="^"):
-  a = float(input("Enter 1st number: "))
-  c = float(input("Enter 2nd number: "))
-  Calculator(b,a,c)
-elif(b=="sqrt" or b=="ln" or b=="log10" or b=="exp" or b=="sin" or b=="cos" or b=="tan"):
-  a = float(input("Enter 1st number: "))
-  Calculator(b,a)
+  b = str(input("Enter the operator: "))
+  if(b=="+" or b=="-" or b=="/" or b=="*" or b=="**" or b=="^"):
+    a = float(input("Enter 1st number: "))
+    c = float(input("Enter 2nd number: "))
+    Calculator(b,a,c)
+  elif(b=="sqrt" or b=="ln" or b=="log10" or b=="exp" or b=="sin" or b=="cos"):
+    a = float(input("Enter the number: "))
+    Calculator(b,a)
+  else:
+    print("Invalid Input!, enter a valid operator")
