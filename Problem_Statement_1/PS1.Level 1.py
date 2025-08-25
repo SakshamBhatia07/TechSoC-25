3f3175b8d15e94bbca9b0e6c95179544d0e3e90d
SakshamBhatia-mnc-iiti <saksham.bhatia0828@gmail.com>
2025-08-25 11:12:03 +0530

    Made the code to code and decode a word by random shift

--- Problem_Statement_1/PS1.Level 1.py
+++ Problem_Statement_1/PS1.Level 1.py
@@ -1,5 +1,4 @@
 l=["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z","A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
-# Shift of 3 has been considered for this code
 b = input("Enter the word to be coded: ")
 c=int(input("Enter 0 to code your word or 1 to decode your code: "))
 a=int(input("Enter the shift value: "))
@@ -7,12 +6,12 @@ match c:
   case 0:
     for char in b:
       try:
-        print(l[l.index(char)+a],end="")
+        print(l[l.index(char)+(a%26)],end="")
       except:
         print(char,end="")
   case 1:
     for char in b:
       try:
-        print(l[l.index(char)+(26-a)],end="")
+        print(l[l.index(char)+(26-(a%26))],end="")
       except:
         print(char,end="")
