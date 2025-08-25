9a2d896901ec56e627223f97fd2e925708e78a75
SakshamBhatia-mnc-iiti <saksham.bhatia0828@gmail.com>
2025-08-25 23:23:38 +0530

    PS1 LEVEL2 UPDATED

--- Problem_Statement_1/PS1.Level 2.py
+++ Problem_Statement_1/PS1.Level 2.py
@@ -24,8 +24,8 @@ while a<=26:
 
   for y in range(0,26,1):
     b=b+(abs(((l1.count(y)/len(l1))*100)-percentage[y]))
-  if (" is " or " and " or " are " or " was " or "were" or " the " or " or " or " they " or " a " or " an " or " be " or " in " or " that " or " have " or " I " or "  it " or " if " or " also " or " who " or " all " or " him " or " its ") in d:
-      b=b-50
+  if (" is " or " and " or " are " or " was " or "were" or " the " or " or " or " they " or " a " or " an " or " be " or " in " or " that " or " have " or " or " or " it " or " if " or " also " or " who " or " all " or " him " or " its ") in d:
+      b=b-30
   if c>b:
     c = b
     f = d
