252fdec59422236dc43049fe192b9ca3fcecc543
SakshamBhatia-mnc-iiti <saksham.bhatia0828@gmail.com>
2025-09-03 00:32:52 +0530

    Some updates

--- Problem_Statement_1/PS1.Level 1.py
+++ Problem_Statement_1/PS1.Level 1.py
@@ -1,5 +1,5 @@
 l=["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z","A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
-b = input("Enter the word to be coded: ")
+b = input("Enter the word to be coded or decoded: ")
 c=int(input("Enter 0 to code your word or 1 to decode your code: "))
 a=int(input("Enter the shift value: "))
 match c:
--- Problem_Statement_1/PS1.Level 3.py
+++ Problem_Statement_1/PS1.Level 3.py
@@ -1,3 +1,4 @@
+#Made the level 3 without the bonus part to decode without keyword
 l = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w",
      "x", "y", "z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t",
      "u", "v", "w", "x", "y", "z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q",
